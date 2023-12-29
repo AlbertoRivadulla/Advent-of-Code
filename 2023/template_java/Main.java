0ac7b76629af31ccac3add603d45ab7dd367c81a
Alberto Rivadulla <albertorivadullasanchez@gmail.com>
2023-12-29 18:33:38 +0100

    Solved 2023/13 in Java

---
+++ 2023/template_java/Main.java
@@ -0,0 +1,47 @@
+import java.time.Instant;
+import java.time.Duration;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+  public static void main( String[] args ) throws Exception {
+
+    // Read the data
+    File inputFile = new File( "input.txt" );
+    Scanner scanner = new Scanner( inputFile );
+
+    while ( scanner.hasNextLine() ) {
+      String thisLine = scanner.nextLine();
+
+      //
+      //
+    }
+
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // First part
+    ////////////////////////////////////////////////////////////////////////////////
+
+    long startTime = System.currentTimeMillis();
+
+
+    System.out.println("\n\n--------------\nFirst part\n--------------");
+    System.out.println("\t--- Execution time: " + (System.currentTimeMillis() - startTime) + " ms ---\n\n");
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Second part
+    ////////////////////////////////////////////////////////////////////////////////
+
+    startTime = System.currentTimeMillis();
+
+
+
+
+    System.out.println("\n\n--------------\nFirst part\n--------------");
+    System.out.println("\t--- Execution time: " + (System.currentTimeMillis() - startTime) + " ms ---\n\n");
+  }
+}
+

53c7bfa609262b71f85e719c83fe643460135c2e
Alberto Rivadulla <albertorivadullasanchez@gmail.com>
2025-12-02 23:24:53 +0100

    Add step to generate compile_commands.json in the Makefile

--- 2025/template/main.c
+++ 2025/template/main.c
@@ -6,41 +6,27 @@
 #include "utils.h"
 #include "timer.h"
 
-void test_timer() {
-    printf("eeeee\n");
+void first_part() {
+    // TODO:
+    return;
 }
 
-// int main(int argc, char* argv[]) {
+void second_part() {
+    // TODO:
+    return;
+}
+
 int main() {
-    clock_t begin = clock();
-
     Timer timer;
-    timer_start(&timer, "Test label");
 
-    printf("Hello from main\n");
-
-
-    int* numbers = malloc(5 * sizeof(int));
-    if (numbers == NULL) {
-        fprintf(stderr, "Memory allocation failure\n");
-        return 1;
-    }
-
-    for (int i = 0; i < 5; ++i) {
-        numbers[i] = i * 5;
-        do_something(i);
-        do_something(numbers[i]);
-    }
-
-    free(numbers);
-
-    clock_t end = clock();
-
-    printf("Execution time: %f s\n", (double)(end - begin) / CLOCKS_PER_SEC);
-
+    timer_start(&timer, "First part");
+    first_part();
     timer_stop(&timer);
 
-    time_function("timed function", test_timer);
+    timer_start(&timer, "Second part");
+    second_part();
+    timer_stop(&timer);
+
 
     return 0;
 }

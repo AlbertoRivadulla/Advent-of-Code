bcdbc682842441a3018ae1daff46eb9797b91d8c
Alberto Rivadulla <albertorivadullasanchez@gmail.com>
2025-12-07 19:20:53 +0100

    Add template for solutions for 2024 in Go

---
+++ 2024/template/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"aoc_2024/lib"
+)
+
+func firstPart(inputFileName string) {
+	defer lib.Timer("First part")()
+
+	file, err := os.Open(inputFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %v\n", inputFileName)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Printf("Line %v\n", line)
+	}
+
+	// TODO:
+}
+
+func secondPart(input_file string) {
+	defer lib.Timer("Second part")()
+
+	// TODO:
+}
+
+func main() {
+	fmt.Println()
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage:\n\t./main <input_file>")
+		os.Exit(1)
+	}
+
+	inputFileName := os.Args[1]
+
+	firstPart(inputFileName)
+
+	secondPart(inputFileName)
+}
